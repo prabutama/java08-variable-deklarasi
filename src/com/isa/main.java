5c040ce7e221c567de2244edca9a71f147cbe79c
prabutama <isaprabutama@gmail.com>
2023-03-19 10:26:45 +0700

    Initial commit

---
+++ src/com/isa/main.java
@@ -0,0 +1,17 @@
+package com.isa;
+
+public class main {
+    public static void main(String[] args) {
+        // Kita akan membuat variable
+        int a = 10; //assignment
+        System.out.println("nilai x = " + a);
+        a = 20;
+        System.out.println("nilai x baru = " + a);
+
+        //Kita akan membuat deklarasi
+        int b; //declarasi
+        b = 7;
+        System.out.println("nilai b = " + b);
+
+    };
+};
